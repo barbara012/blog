40d6432d37da0f3fbe2ee2357948d6e3f39edd83
huwenhua <huwenhua8901@163.com>
2014-10-28 23:13:59 +0800

    update

---
+++ public/js/userMore.js
@@ -0,0 +1,1 @@
+!function(){var t=$(".user-center"),e=$("<div></div>"),n=$("<a></a>").text("设置"),r=$("<a></a>").text("个人中心"),i=$('<a href="/logout"></a>').text("退出");e.append(n).append(r).append(i),PopTip.showPop(t,"click",e,null,null,"default")}();
