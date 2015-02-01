650083e3d60d0633d419b66f6e8e41f45dfeeb4e
GilbertSun <szb4321@gmail.com>
2015-02-01 20:23:51 +0800

    add less support

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,21 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var less = require('gulp-less');
 
 gulp.task('release', function () {
     gulp.src(['./public/js/*.js', '!./public/js/*.min.js'])
         .pipe(uglify())
         .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('less', function () {
+    gulp.src(['./public/stylesheets/*.less'])
+        .pipe(less({
+            compress: true
+        }))
+        .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('coding', function () {
+
 });
